cded0704523e15c5046dddd3202e4c786735b3fa
Jonathan Kerk <contact@jonathankerk.com>
2023-12-18 23:20:34 +0800

    feat: refactored routes into inline functions (temporarily)

--- main.go
+++ main.go
@@ -2,109 +2,24 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 
 	"log"
 
 	"github.com/gin-gonic/gin"
-
-	"github.com/go-sql-driver/mysql"
+	"github.com/jonzxz/jfc/constants"
+	"github.com/jonzxz/jfc/models"
 )
 
 var (
-	//db *sql.DB
-	cfg = mysql.Config{
-		User:   "bizuser",
-		Passwd: "secretpassword",
-		Net:    "tcp",
-		Addr:   "localhost:3306",
-		DBName: "JFC",
-	}
+	db *sql.DB
 )
-
-type Person struct {
-	ID         int    `json:"ID"`
-	Name       string `json:"Name"`
-	TelegramId string `json:"TelegramId"`
-}
-
-type Payment struct {
-	ID int `json:"ID"`
-	// pass in as string, convert to uint64 epoch
-	Date    string `json:"Date"`
-	Remarks string `json:"Remarks"`
-}
-
-type PaymentDue struct {
-	ID int `json:"ID"`
-	// FK Person.ID
-	Payer         int    `json:"Payer"`
-	PaymentDue    string `json:"PaymentDue"`
-	PayableAmount int    `json:"PayableAmount"`
-	Paid          bool   `json:"Paid"`
-}
-
-//var people = []Person{
-//{ID: 1, Name: "Jonny", TelegramId: "@jonny"},
-//{ID: 2, Name: "Sally", TelegramId: "@sally"},
-//}
-
-func getPeople(c *gin.Context) {
-
-	people := getAllPersonsHandler()
-	c.IndentedJSON(http.StatusOK, people)
-}
-
-func getAllPersonsHandler() []Person {
-	db, err := sql.Open("mysql", cfg.FormatDSN())
-
-	if err != nil {
-		log.Fatalf("err %v\n", err)
-		return nil
-	}
-
-	defer db.Close()
-
-	results, err := db.Query("SELECT * FROM PERSON")
-
-	if err != nil {
-		log.Fatalf("err %v\n", err)
-		return nil
-	}
-
-	people := []Person{}
-	for results.Next() {
-		var t Person
-		err = results.Scan(&t.ID, &t.Name, &t.TelegramId)
-
-		if err != nil {
-			log.Fatalf("err %v\n", err)
-		}
-
-		people = append(people, t)
-	}
-
-	return people
-
-}
-
-//func addPeople(c *gin.Context) {
-//var newPerson Person
-
-//if err := c.BindJSON(&newPerson); err != nil {
-//return
-//}
-
-//people = append(people, newPerson)
-//c.IndentedJSON(http.StatusCreated, newPerson)
-
-//}
 
 func main() {
 
-	// use os.Getenv instead
-
-	db, err := sql.Open("mysql", cfg.FormatDSN())
+	db, err := sql.Open("mysql", constants.DB_CONFIGS.FormatDSN())
+	fmt.Printf("%p\n", db)
 
 	if err != nil {
 		log.Fatal(err)
@@ -117,8 +32,20 @@ func main() {
 	log.Println("Connected to db!")
 
 	router := gin.Default()
-	router.GET("/people", getPeople)
-	//router.POST("/add", addPeople)
+	//router.GET("/people", getPeople)
+	router.GET("/people", func(c *gin.Context) {
+		people := models.GetAllPersonsHandler(db)
+		c.IndentedJSON(http.StatusOK, people)
+
+	})
+	router.POST("/add", func(c *gin.Context) {
+		var newPerson models.Person
+		if err := c.BindJSON(&newPerson); err != nil {
+			log.Fatalf("%v\n", err)
+		}
+		models.AddPersonHandler(db, newPerson)
+		c.IndentedJSON(http.StatusCreated, newPerson)
+	})
 
 	router.Run("localhost:8080")
 
