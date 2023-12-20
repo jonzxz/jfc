c7c26ddb5acc4add333c5576e136ac6498856dd6
Jonathan Kerk <contact@jonathankerk.com>
2023-12-20 11:35:57 +0800

    feat: added handler to get all payments, updated data types for sql tables

--- models/Payment.go
+++ models/Payment.go
@@ -1,9 +1,35 @@
 package models
 
+import (
+	"database/sql"
+	"log"
+)
+
 type Payment struct {
 	ID int `json:"ID"`
 	// pass in as string, convert to uint64 epoch
-	Date        string  `json:"Date"`
+	Timestamp   int64   `json:"Timestamp"`
+	Type        string  `json:"Type"`
 	Remarks     string  `json:"Remarks"`
 	TotalAmount float32 `json:"TotalAmount"`
+}
+
+func GetAllPaymentsHandler(db *sql.DB) []Payment {
+	results, err := db.Query("SELECT * FROM PAYMENT")
+
+	if err != nil {
+		log.Fatalf("err %v\n", err)
+		return nil
+	}
+
+	payments := []Payment{}
+
+	for results.Next() {
+		var t Payment
+		err = results.Scan(&t.ID, &t.Timestamp, &t.Type, &t.Remarks, &t.TotalAmount)
+		payments = append(payments, t)
+	}
+
+	return payments
+
 }
