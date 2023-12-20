e1834d022692b88e1d90190313403fd2f3d3f393
Jonathan Kerk <contact@jonathankerk.com>
2023-12-20 13:25:07 +0800

    fix: end epoch time

--- models/Payment.go
+++ models/Payment.go
@@ -100,7 +100,8 @@ func getStartEndEpochFromMonth(month string) map[string]int64 {
 	monthTime := time.Month(monthInt)
 	currentLocation := now.Location()
 	firstOfMonth := time.Date(currentYear, monthTime, 1, 0, 0, 0, 0, currentLocation)
-	lastOfMonth := firstOfMonth.AddDate(0, 1, -1)
+	// dirty add to make 23:59:59
+	lastOfMonth := firstOfMonth.AddDate(0, 1, -1).UTC().Add(time.Hour * 23).Add(time.Minute * 59).Add(time.Second * 59)
 
 	startUnix := firstOfMonth.Unix()
 	endUnix := lastOfMonth.Unix()
